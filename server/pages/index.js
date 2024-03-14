0baf5a4470f7f31f94212c410adb1109dc47cd10
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-14 17:27:05 +0100

    worked on main page

--- server/pages/index.js
+++ server/pages/index.js
@@ -15,11 +15,34 @@ export default function Home() {
                 <link rel="icon" href="icon.responsive.svg" />
                 <link rel="apple-touch-icon" href="icon.responsive.svg" />
                 <link rel="stylesheet" href="/css/main.css" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" />
             </Head>
-            <main className={styles.container}>
-                {`This is some text bla bla bla bla`}
-                <img src="/icon.responsive.svg" />
+            <main id={styles["main-content"]}>
+                <section id="Pirates-of-the-Caribbean-At-Campus-End">
+                    <img id={"site-icon"} src="/icon.responsive.svg" />
+                    <h1 id={styles["main-title"]}> 
+                        Pirates of the Caribbean: At Campus' End
+                    </h1>
+                    <p>
+                        This is some text bla bla bla bla
+                    </p>
+                </section>
+                <section id={"introduction"}>
+                    <h2>introduction</h2>
+                    <p>
+                        In this part, we'll desribe how to play the game
+                    </p>
+                </section>
             </main>
+            <footer id={"footer"}>
+                Copyright &copy; 
+                {(new Date().getFullYear() === 2024) ? (`2024`) : (`2024 - ${new Date().getFullYear()}`)}&nbsp;
+                &middot; Group 4 for Game Design
+                &middot; All rights reserved
+            </footer>
         </>
     );
 }
