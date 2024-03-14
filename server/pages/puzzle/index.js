b5ff879f7f75f2912c67055f0a30a530f14f46cc
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-14 16:01:54 +0100

    added a puzzle page

---
+++ server/pages/puzzle/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import styles from '../../styles/css/Puzzle.module.min.css';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Puzzle | Pirates of the Caribbean: At Campus' End</title>
+                <meta charset="UTF-8" />
+                <meta name="darkreader-lock" />
+                <meta name="author" content="Tygo van den Hurk" /> 
+                {/* Feel free to add yourself to the list. */}
+                <meta name="description" content="On this page you can see the puzzle. Solve the puzzle, find the treasure, get the price!"  />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="icon.responsive.svg" />
+                <link rel="apple-touch-icon" href="icon.responsive.svg" />
+                <link rel="stylesheet" href="/css/main.css" />
+            </Head>
+            <main className={styles.container}>
+
+            </main>
+        </>
+    );
+}
