b26e501c4bfc15f4947c6eb0e0b0a520b93ff67a
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-14 16:01:30 +0100

    created a maps page.

---
+++ server/pages/map/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import styles from '../../styles/css/Map.module.min.css';
+
+export default function Map() {
+    return (
+        <>
+            <Head>
+                <title>Map | Pirates of the Caribbean: At Campus' End</title>
+                <meta charset="UTF-8" />
+                <meta name="darkreader-lock" />
+                <meta name="author" content="Tygo van den Hurk" /> 
+                {/* Feel free to add yourself to the list. */}
+                <meta name="description" content="This is the map where you can see where the treasure is. Solve the puzzle, find the treasure, get the price!"  />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="icon.responsive.svg" />
+                <link rel="apple-touch-icon" href="icon.responsive.svg" />
+                <link rel="stylesheet" href="/css/main.css" />
+            </Head>
+            <main className={styles.container}>
+
+                {/* {Utils} */}
+            </main>
+        </>
+    );
+}
