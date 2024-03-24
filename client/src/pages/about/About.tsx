da3c8bbed2fd839620e20a4b84b17551b45faba9
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-24 22:39:13 +0100

    added an about page.

---
+++ client/src/pages/about/About.tsx
@@ -0,0 +1,25 @@
+import { repositoryURL, urlTygo, urlLeo, urlRuben, urlEryk, contactURL } from "./../constants";
+
+function About() {
+    return (
+        <>
+            <h1> About </h1>
+            <p>
+                The website, game, and idea were designed, and created with love for Game Design III, a course from 
+                TU/e by group 4. Consisting 
+                of <a href={urlTygo} >Tygo van den Hurk</a>, 
+                <a href={urlLeo} > Leonidas Hadjiyiannis</a>, 
+                <a href={urlRuben} > Ruben Savelkouls</a>, and
+                <a href={urlEryk} > Eryk Kiepuszewski</a>.
+            </p>
+            <p>
+                The source code is available on <a href={repositoryURL}>GitHub</a> for your entertainment. If you have any
+                questions, or concerns, you 
+                can <a href={contactURL}>contact me</a>.
+            </p>
+        </>
+    );
+}
+
+
+export default About;
