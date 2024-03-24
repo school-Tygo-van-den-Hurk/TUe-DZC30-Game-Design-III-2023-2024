63f57230ac5c4c8258745f51d16fed1ca5b4ef97
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-24 17:58:33 +0100

    Worked on the maps page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,33 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { NavLink as ReactRouterNavLink, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Maps from './pages/maps/Maps';
 import Home from './pages/home/Home';
 import Puzzle from './pages/puzzle/Puzzle';
 
+function NavLink(props: { to: string; children: React.ReactNode; }) {
+    const { pathname } = useLocation();
+    const isActive = pathname === props.to;
+    const className = (isActive) ? ("active-nav-link") : ("");
+
+    return (
+        <ReactRouterNavLink {...props} className={className} />
+    );
+}
+
 function App() {
     return (
         <div className="App">
-            <nav>
-                <Link to="/maps">to the maps page</Link>
-                <Link to="/puzzle">to the puzzle</Link>
-                <Link to="/">home page</Link>
-            </nav>
+            <div className="nav-bar">
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/puzzle">Puzzle</NavLink>
+                    <NavLink to="/maps">Map</NavLink>
+                </nav>
+            </div>
             <main>
                 <Routes>
-                    <Route path="/maps" element={ <Maps/> } />
                     <Route path="/" element={ <Home/> } />
+                    <Route path="/maps" element={ <Maps/> } />
                     <Route path="/puzzle" element={ <Puzzle/> } />
                 </Routes>
             </main>
--- client/src/pages/maps/Maps.tsx
+++ client/src/pages/maps/Maps.tsx
@@ -1,41 +1,103 @@
 import axios from 'axios';
 import useSWR from 'swr';
+import "./Maps.css";
 
-const fetcher = (url:any) => axios.get(url).then(res => res.data);
+const https = false;
+const port = 3001;
+const domain = "localhost";
+const path = "api/coordinates";
 
+/**
+ * This function stores the 
+ * @returns 
+ */
+function getTextAndTitle() {
+    return (
+        <>
+            <h1> This is the Maps page </h1>
+            <p>
+                In this section you can look at the map and get an idea of where the treasure might be.
+                There is a radius in which the treasure could reside, and every so often the radius in
+                which the treasure will shrink to make it easier to find it. Whenever some one does 
+                find it, then the radius resets and the treasure is moved.
+            </p>
+        </>
+    );
+}
+
+/**
+ * The maps page has to load in the coordinates from the backend. That's why it has three states:
+ * - loading,
+ * - failure,
+ * - success.
+ * 
+ * Depending on the state, a different state must be displayed.
+ * 
+ * @returns the maps page.
+ */
 function Maps() {
 
-    const {
-        data: coordinates,
-        error,
-        isValidating,
-    } = useSWR('http://localhost:3001/api/coordinates', fetcher);
+    const url:string = (
+        `http${https?'s':''}://${domain}:${port}/${path.startsWith('/')?path.substring(1,path.length):path}`);
+    const fetcher = (url:string) => axios.get(url).then(res => res.data);
+    const { data: coordinates, error, isValidating } = useSWR(url, fetcher);
 
     if (error) return (
-        <div className='failed'>
-            <p>
-                failed to load coordinates. 
-                {/* Error: {error}. */}
-            </p>
-        </div>
+        <>
+            {getTextAndTitle()}
+            {failed(error)}
+        </>
     );
-
+    
     if (isValidating) return (
-        <div className="Loading">
-            <p>
-                Loading coordinates...
-            </p>
-        </div>
+        <>
+            {getTextAndTitle()}
+            {loading()}
+        </>
     );
 
     return (
-      <div>
-        <h1>This is the Home page</h1>
-        <p>
-            these are the coordinates: {JSON.stringify(coordinates)}
-        </p>
-      </div>
+        <>
+            {getTextAndTitle()}
+            {success(coordinates)}
+        </>
     );
+}
+
+/**
+ * this function returns the HTML that will be displayed when the coordinates are retrieved successfully.
+ * 
+ * @param coordinates the coordinates of the box.
+ * @returns the HTML on a successful load.
+ */
+function success(coordinates:{lat:number,lon:number}) {
+    return (
+        <>
+            <p className="success"> The coordinates loaded successfully. </p>
+            <p> The coordinates are latitude: {coordinates.lat}, and longitude: {coordinates.lon}. </p>
+            <div className="map-container layer-0"> This is a placeholder for the map... </div>
+        </>
+    );
+}
+
+/**
+ * this function returns the HTML that will be displayed when the coordinates are loading.
+ *
+ * @returns the HTML that will be displayed when the coordinates are loading.
+ */
+function loading() {
+    return ( <p className="loading"> Loading the coordinates... </p> );
+}
+
+/**
+ * this function returns the HTML that will be displayed when the coordinates failed to load.
+ * 
+ * @param error the `error` that occurred.
+ * @returns the HTML to display on a fail.
+ */
+function failed(error?:any) {
+    console.error(error);
+    return ( <p className="failed"> failed to load the coordinates. </p> );
 }
 
 export default Maps;
