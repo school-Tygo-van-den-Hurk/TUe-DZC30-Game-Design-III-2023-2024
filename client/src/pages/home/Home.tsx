80837c570615d3302ebd7741851db125cd25ea52
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-24 22:38:27 +0100

    worked on the home page.

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
+// TODO : @Leo add some text to the home page saying what this is, and how to play, etc etc the more the better.
+import "./home.css"
+
 function Home() {
     return (
         <>
-            <h1> This is the Home page </h1>
+            <h1> Home </h1>
             <p>
                 Some text about what this game is
             </p>
+            <br /><br /><hr />
+            <h2> some subject </h2>
+            <p> text about this subject </p>
+            <h3> some sub-subject </h3>
+            <p> and some text about it.... </p>
         </>
     );
 }
