5fade6367ee58fc477c7d045161a80f8040fae2c
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-24 22:34:58 +0100

    updated the routing

--- server/index.js
+++ server/index.js
@@ -1,24 +1,36 @@
 "use strict"; //! @Eryk do not touch this file unless absolutely needed.
 
-import getLocation from "./get-location.js";
-import express from "express";
+import getLocation from "./maps.js";
+import express, { response } from "express";
 import cors from "cors";
 import print from "./Printer.js";
+import { lastSolve, getPuzzle } from "./puzzle.js";
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 const app = express();
 app.use(cors())
+app.use((request, _response, next) => {
+    
+    const clientIP = (request.ip || request.connection.remoteAddress);
+    const path = (request.path);
 
-const pathToCoordinate = ("/api/coordinates");
-app.get("/", (_request, response) => { 
-    print(`Got request from \"${requesterIP}\" for \"/\".`);
-    response.send(`go to \"${pathToCoordinate}\" for the coordinates.`); 
+    print(`Got request from \"${clientIP}\" for \"${path}\".`);
+
+    next();
 });
 
-app.get(pathToCoordinate, (request, response) => { 
-    const requesterIP = (request.socket.remoteAddress);
-    print(`Got request from \"${requesterIP}\" for \"${pathToCoordinate}\".`);
-    response.json(getLocation()) 
-});
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+const pathToCoordinate = ("/maps");
+const pathToPuzzle = ("/puzzle");
+const defaultResponse = (`Go to \"${pathToPuzzle}\", or \"${pathToCoordinate}\".`);
+
+app.get("/",              (_request, response) => response.send(defaultResponse) );
+app.get(pathToPuzzle,     (_request, response) => response.json(getPuzzle()) );
+app.get(pathToCoordinate, (_request, response) => response.json(getLocation()) );
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 const port = (3001);
 app.listen(port);
