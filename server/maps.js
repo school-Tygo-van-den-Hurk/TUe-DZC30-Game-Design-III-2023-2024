08ffb5cbb22b2bd4f32e6456684b3e3b6a0285a9
Tygo van den Hurk <hazels.05.remount@icloud.com>
2024-03-24 22:34:41 +0100

    renamed 'get-location.js' to 'maps.js' to better match the pattern of the files reflecting the page they're supporting.

---
+++ server/maps.js
@@ -0,0 +1,20 @@
+"use strict"; //~ @Eryk Go ahead and modify this file to your hearts content.
+
+import print from "./Printer.js";
+
+/**
+ * Get's and returns the location from the GPS module.
+ */
+export default function getLocation() {
+
+    // TODO : Implement GPS COORDINATES USING: 
+    // https://dzone.com/articles/read-gps-data-with-a-raspberry-pi-zero-w-and-nodej
+
+    // print("Some print statement."); // use this print function for non debugging, use console.log for debugging.
+    return ({
+        coordinates: { lat:420.0, lon:6.9 },
+        lastSolved: {
+            
+        }
+    });
+}
