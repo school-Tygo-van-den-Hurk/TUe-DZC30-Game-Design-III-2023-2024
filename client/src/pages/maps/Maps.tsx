d61c1ae40f35f4c14f6231cc2fba002bb5f69271
Eric Kiepushevski <e.b.kiepuszewski@student.tue.nl>
2024-04-04 16:37:22 +0200

    Circle on map works

--- client/src/pages/maps/Maps.tsx
+++ client/src/pages/maps/Maps.tsx
@@ -1,41 +1,40 @@
-import "./Maps.css"; 
+import "./Maps.css";
 import axios from 'axios';
 import useSWR from 'swr';
 import server from "../../assets/server";
-import GoogleMap, { GoogleMapProps } from "../../assets/GoogleMap/GoogleMap";
+import CustomGoogleMap, { GoogleMapProps } from "../../assets/GoogleMap/GoogleMap";
+//import { Circle } from "@react-google-maps/api";
 
 // TODO REMOVE WHEN AN ACTUAL IMPLEMENTATION IS HERE
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // # BEGIN TODO : remove when you have an actual implementation
-function getTempProps(_data:BackendCoordinateRequestResult) { 
-        
-    const marker = ({ 
-        color:"#00FF00",
-        position:{ lat:51.44904336679616, lng:5.487273789974496 }
+function getTempProps(_data: BackendCoordinateRequestResult) {
+
+    const marker = ({
+        color: "#FFD700",
+        position: { lat: 51.44904336679616, lng: 5.487273789974496 }
     });
 
     const circle = {
-        options: { 
-            strokeColor:"#00FF00",         
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#FF0000",
-            fillOpacity: 0.35,
-        },
-        radius:4000,
-        center:{ lat:51.44904336679616, lng:5.487273789974496 }
+        strokeColor: "#FFD700",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FFD700",
+        fillOpacity: 0.35,
+        center: { lat: 51.44904336679616, lng: 5.487273789974496 },
+        radius: 20
     }
 
-    const props:GoogleMapProps = {
-        apiKey:"AIzaSyCJET1X_nGd5nsC1RLv1YTUWFg9HPkAjxQ", 
+    const props: GoogleMapProps = {
+        apiKey: "AIzaSyCJET1X_nGd5nsC1RLv1YTUWFg9HPkAjxQ",
         map: {
-            position:{
+            position: {
                 lat: 51.448555556, //// data.coordinates.lat, 
                 lng: 5.489083333   ////data.coordinates.lon
             }
         },
-        marker:marker,
-        circle:circle 
+        marker: marker,
+        circle: circle
     }
 
     return (props);
@@ -46,8 +45,8 @@ function getTempProps(_data:BackendCoordinateRequestResult) {
 const path = "maps";
 
 interface BackendCoordinateRequestResult {
-    coordinates:{ lat:number, lng:number }
-    lastSolved:string
+    coordinates: { lat: number, lng: number }
+    lastSolved: string
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
@@ -57,14 +56,13 @@ interface BackendCoordinateRequestResult {
  * @param coordinatesTreasure 
  * @returns 
  */
-function getMap(data:BackendCoordinateRequestResult) {
-
-    return ( 
+function getMap(data: BackendCoordinateRequestResult) {
+    return (
         // TODO : @Leo use the google maps API to create a map here.
         // Search for terms 'node js', 'google maps API', and 'react' for better results.
         <div className="map-container layer-0" >
-            <GoogleMap {...getTempProps(data)}/>
-        </div>
+            <CustomGoogleMap {...getTempProps(data)} />
+        </div >
     );
 }
 
@@ -81,7 +79,7 @@ function getTextAndTitle() {
             <p>
                 In this section you can look at the map and get an idea of where the treasure might be.
                 There is a radius in which the treasure could reside, and every so often the radius in
-                which the treasure will shrink to make it easier to find it. Whenever some one does 
+                which the treasure will shrink to make it easier to find it. Whenever some one does
                 find it, then the radius resets and the treasure is moved.
             </p>
         </>
@@ -100,8 +98,8 @@ function getTextAndTitle() {
  */
 function Maps() {
 
-    const url:string = (`${server.getURL()}${path}`);
-    const fetcher:(url:string)=>Promise<any> = (url:string) => axios.get(url).then(res => res.data);
+    const url: string = (`${server.getURL()}${path}`);
+    const fetcher: (url: string) => Promise<any> = (url: string) => axios.get(url).then(res => res.data);
     const { data, error, isValidating } = useSWR(url, fetcher);
 
     if (error) return (
@@ -110,7 +108,7 @@ function Maps() {
             {getTextAndTitle()}
         </>
     );
-    
+
     if (isValidating) return (
         <>
             {loading()}
@@ -133,16 +131,16 @@ function Maps() {
  * @param coordinates the coordinates of the box.
  * @returns the HTML on a successful load.
  */
-function success(data:BackendCoordinateRequestResult) {
+function success(data: BackendCoordinateRequestResult) {
     return (
         <>
             <p className="success"> The coordinates loaded successfully. </p>
-            <p> 
-                The coordinates are latitude: {data.coordinates.lat}, and longitude: {data.coordinates.lon}. 
+            <p>
+                The coordinates are latitude: {data.coordinates.lat}, and longitude: {data.coordinates.lon}.
                 this will be removed when we launch the website.
             </p>
         </>
-    );  
+    );
 }
 
 /**
@@ -150,12 +148,12 @@ function success(data:BackendCoordinateRequestResult) {
  *
  * @returns the HTML that will be displayed when the coordinates are loading.
  */
-function loading() { 
+function loading() {
 
-    return ( 
-        <p className="loading"> 
-            Loading the coordinates... 
-        </p> 
+    return (
+        <p className="loading">
+            Loading the coordinates...
+        </p>
     );
 }
 
@@ -165,14 +163,14 @@ function loading() {
  * @param error the `error` that occurred.
  * @returns the HTML to display on a fail.
  */
-function failed(error?:any) {
-    
+function failed(error?: any) {
+
     console.error(error);
 
-    return ( 
+    return (
         <p className="failed">
-            failed to load the coordinates. 
-        </p> 
+            failed to load the coordinates.
+        </p>
     );
 }
 
