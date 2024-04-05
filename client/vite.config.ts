96cea5e2fdf59fb8c071ed3276c865aaeb88a0de
St-h123 <hazels.05.remount@icloud.com>
2024-04-05 13:43:23 +0000

    fixed docker again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: "./",
     server: {
         host: true,
         port: 4173,
         watch: {
             usePolling: true
         }
+    },
+    preview: {
+        host: true,
+        port: 4173,
     }
 });
